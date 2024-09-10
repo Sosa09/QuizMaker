57cfb5e98cbaf83c0b8652516582b2859603cce0
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-09-10 19:32:25 +0200

    Created participant class to hold the particpant data such as name age and his result from preivopus created class ParticipantResult

---
+++ QuizMaker/Participant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaker
+{
+    public class Participant
+    {
+        public string Name;
+        public int Age;
+        public ParticipantResult Result;
+        public Participant() 
+        {
+            
+        }
+
+    }
+}
